f81b42e5531ee6f7caedaaa1ef2e8663c7f38a19
zwyqz <329636271@qq.com>
2017-01-02 22:41:44 +0800

    arena修改

---
+++ src/test/java/io/mycat/bigmem/buffer/ArenaTest.java
@@ -0,0 +1,35 @@
+
+package io.mycat.bigmem.buffer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+*@desc
+*@author zhangwy   @date 2016年12月31日 上午7:50:53
+**/
+public class ArenaTest {
+	@Test
+	public void testTinyOrSmall() {
+//		Arena<DirectByteBuffer> arena = new DirectByteBuffer(8192,16 * 1024 * 1024 , 13);
+//		System.out.println(arena.isTinyOrSmall(512));
+//		for(int i = 0 ; i < 8192; i++) {
+//		//	Assert.assertTrue(!arena.isTinyOrSmall(i));
+//			if(!arena.isTinyOrSmall(i)) {
+//				System.out.println("isTinyOrSmall function is wrong");
+//			}
+//		}
+	}
+	
+	@Test
+	public void testisTiny() {
+//		Arena<DirectByteBuffer> arena = new DirectByteBuffer(8192);
+//		for(int i = 0 ; i < 519; i++) {
+//		//	Assert.assertTrue(!arena.isTinyOrSmall(i));
+//			if(!arena.isTiny(i)) {
+//				System.out.println(String.format("%d is not tiny", i));
+//			}
+//		}
+	}
+}
+
---
+++ src/test/java/io/mycat/bigmem/buffer/DirectByteBufferTest.java
@@ -0,0 +1,27 @@
+
+package io.mycat.bigmem.buffer;
+
+import java.nio.ByteBuffer;
+
+import org.junit.Test;
+
+import sun.nio.ch.DirectBuffer;
+
+/**
+*@desc
+*@author zhangwy   @date 2017年1月2日 下午6:02:44
+**/
+public class DirectByteBufferTest {
+	@Test
+	public void testNewInstance() {
+		BaseByteBuffer<DirectBuffer> buffer = null;
+		Arena<DirectBuffer> arena = new DirectArena(8192,16 * 1024 * 1024 , 13);
+		//Chunk<DirectBuffer> chunk = arena.newChunk();
+		//buffer.init(chunk , 0, 0,300, 300);
+		//buffer._putByte(0, (byte)9);
+		buffer = arena.allocateBuffer(300);
+		buffer._putByte(0, (byte)9);
+		System.out.println(buffer._getByte(0));
+	}
+}
+
